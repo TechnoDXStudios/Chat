ea92fce03d5399598de5362010ac78e53ec0d2de
EgorkaStudios <EgorkaStudios@ya.ru>
2019-11-07 20:22:01 +1000

    Some Updates

--- client.py
+++ client.py
@@ -2,11 +2,28 @@ from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import tkinter
 
+HOST = input("Введите адрес сервера(можно узнать у человека, который запустил сервер): ")
+if HOST == "1":
+    HOST = "127.0.0.1"
+PORT = 33000
+if not PORT:
+    PORT = 33000
+else:
+    PORT = int(PORT)
+
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+if HOST != "0":
+    client_socket = socket(AF_INET, SOCK_STREAM)
+
 def receive():
     while True:
         try:
             msg = client_socket.recv(BUFSIZ).decode("utf8")
-            msg_list.insert(tkinter.END, msg)
+            msg = msg.split("▓")
+            msg_list.insert(tkinter.END, msg[0])
+            msg_list.itemconfig("end", fg = msg[1])
         except OSError:
             break
 
@@ -27,16 +44,16 @@ def on_closing(event=None):
 top = tkinter.Tk()
 top.title("Окно чата")
 top.geometry("1024x340")
-top.configure(bg = "#282c34")
+top.configure(bg = "#151515")#"#282c34")
 
 messages_frame = tkinter.Frame(top)
 my_msg = tkinter.StringVar()
 scrollbar = tkinter.Scrollbar(messages_frame)
-entry_field = tkinter.Entry(top, textvariable=my_msg, background = "#21252b", width = 150, fg = "#dddddd", borderwidth = 0, insertbackground = "#2277dd", font='Consolas 12')
+entry_field = tkinter.Entry(top, textvariable=my_msg, background = "#21252b", width = 150, fg = "#dddddd", borderwidth = 0, insertbackground = "#2277dd", font='Consolas 12', selectbackground='#333a46')
 entry_field.bind("<Return>", send)
 entry_field.pack()
 msg_list = tkinter.Listbox(messages_frame, height=15, width=150, yscrollcommand=scrollbar.set)
-msg_list.configure(fg = "#dddddd", bg = "#282c34", highlightbackground = "#282c34", highlightcolor = "#282c34", borderwidth = 0, font='Consolas 10')
+msg_list.configure(fg = "#dddddd", bg = "#282c34", highlightbackground = "#282c34", highlightcolor = "#282c34", borderwidth = 0, font='Consolas 11', selectmode='multiple', selectbackground='#333a46')
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 msg_list.pack()
@@ -47,18 +64,8 @@ send_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
 
-HOST = input("Введите адрес сервера(можно узнать у человека, который запустил сервер): ")
-PORT = 33000
-if not PORT:
-    PORT = 33000
-else:
-    PORT = int(PORT)
-
-BUFSIZ = 1024
-ADDR = (HOST, PORT)
-
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect(ADDR)
+if HOST != "0":
+    client_socket.connect(ADDR)
 
 receive_thread = Thread(target=receive)
 receive_thread.start()
--- server.py
+++ server.py
@@ -7,30 +7,49 @@ def accept_incoming_connections():
  while True:
      client, client_address = SERVER.accept()
      print("%s:%s has connected." % client_address)
-     client.send(bytes("Добро пожаловать, введите своё имя", "utf8"))
+     client.send(bytes("Добро пожаловать, введите своё имя▓#84c257", "utf8"))
      addresses[client] = client_address
      Thread(target=handle_client, args=(client,)).start()
 
+def interpret(s):
+    if "(" in s and ")" in s:
+        s = s[: s.find("(")].replace(" ", "") + s[s.find("("):]
+        com = s[1:s.find("(")]
+        arg = s[s.find("(") + 1: s.find(")")].split(",")
+    else:
+        s = s.replace(" ", "")
+        com = s[1:].replace(" ", "")
+        arg = []
+    return [com, arg]
 
 def handle_client(client):  
 
- name = client.recv(BUFSIZ).decode("utf8")
- welcome = 'Добро пожаловать %s! Если вы захотите выйти, напишите (выход)' % name
- client.send(bytes(welcome, "utf8"))
- msg = "%s has joined the chat!" % name
- broadcast(bytes(msg, "utf8"))
- clients[client] = name
+    name = client.recv(BUFSIZ).decode("utf8")
+    welcome = 'Добро пожаловать %s! Если вы захотите выйти, напишите (выход)▓#317fe3' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!▓#dddddd" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+    color = "#dddddd"
 
- while True:
-     msg = client.recv(BUFSIZ)
-     if msg != bytes("(выход)", "utf8"):
-         broadcast(msg, name+": ")
-     else:
-         client.send(bytes("(выход)", "utf8"))
-         client.close()
-         del clients[client]
-         broadcast(bytes("%s покинул чат." % name, "utf8"))
-         break
+    while True:
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("(выход)", "utf8"):
+            mes = msg.decode("utf-8")
+            if mes[0] != ">":
+                broadcast(bytes(name + ": " + mes + "▓" + color, "utf8"))
+            else:
+                get = interpret(mes)
+                print(get)
+                if get[0] == "color":
+                    color = get[1][0].replace(" ", "")
+                    print(color)
+        else:
+            client.send(bytes("(выход)", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s покинул чат.▓#992c34" % name, "utf8"))
+            break
 
 
 def broadcast(msg, prefix=""): 
@@ -51,7 +70,7 @@ SERVER = socket(AF_INET, SOCK_STREAM)
 SERVER.bind(ADDR)
 
 if __name__ == "__main__":
- SERVER.listen(5)
+ SERVER.listen(10)
  print("Ждёим подключений! ...")
  ACCEPT_THREAD = Thread(target=accept_incoming_connections)
  ACCEPT_THREAD.start()
